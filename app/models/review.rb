3eec5f4800167d7cba866373fa0b582b2dd94a5f
Lilian Li <lil712@hotmail.com>
2022-04-26 22:10:53 +0800

    added validations to listing model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :rating, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
 end
