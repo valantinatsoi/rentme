c21ea390d72b058b5b639738970facd9d388e296
Lilian Li <lil712@hotmail.com>
2022-04-26 21:12:56 +0800

    bookings controller methods added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,40 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:edit, :update]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @listing = Listing.find(params[:id])
+    @booking.listing = @listing
+
+    if @booking.save
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: "Booking was succesfully updated"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:listing_id, :user_id, :amount, :status, :requirements)
+  end
 end
