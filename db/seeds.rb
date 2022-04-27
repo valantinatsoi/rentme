f3e779b5268961bad41ab13e282dcbc7326cd4be
Lilian Li <lil712@hotmail.com>
2022-04-27 15:36:13 +0800

    made changes on the homescreen

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+
+Category.create(name: "personal", image_url: "https://images.unsplash.com/photo-1518894347072-3bfedb006095?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80", description: "Need a stand-in boyfriend for your next CNY gathering?")
+
+Category.create(name: "professional", image_url: "https://images.unsplash.com/photo-1565728744382-61accd4aa148?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80", description: "Find a stand-in secretary for your next meeting!")
