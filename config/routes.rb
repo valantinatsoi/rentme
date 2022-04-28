37a6649974c511c56dda6d68175f47cb2f2d1d67
Lilian Li <lil712@hotmail.com>
2022-04-28 14:40:00 +0800

    create listings new working within each category

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   root to: 'categories#index'
   get 'profile', to: 'pages#profile', as: :profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [ :index, :show ]
-  resources :listings do
+  resources :categories, only: [ :index, :show ] do
+    resources :listings, only: [ :new, :create ]
+  end
+  resources :listings, only: [ :show, :edit, :update, :destroy ] do
     resources :bookings, only: [ :new, :create ]
   end
-  resources :bookings, only: [ :edit, :update ] do
+  resources :bookings, only: [ :show, :edit, :update, :destroy ] do
     resources :reviews, only: [ :new, :create ]
   end
 end
