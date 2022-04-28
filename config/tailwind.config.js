a3fde2397946f969f94f115aecb006d78129b6f5
Lilian Li <lil712@hotmail.com>
2022-04-28 23:34:49 +0800

    homepage edited

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Proxima Nova', ...defaultTheme.fontFamily.sans],
       },
     },
   },
