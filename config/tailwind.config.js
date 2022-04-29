bffabb1d67389fe4b1c2d98e9ac515b8da22c298
Lilian Li <lil712@hotmail.com>
2022-04-29 11:36:53 +0800

    changed cards and colors/fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Quicksand', ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'hotpink': '#FF008E',
+        'headings': '#0C1E7F'
       },
     },
   },
